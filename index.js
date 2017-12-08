e45769f868e607914b2d20222d03fdd1b797fcd1
Cavit Keskin <cavit@truefacet.com>
2017-12-08 15:04:04 -0500

    Update index.js

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ const server = http.createServer((req, res, next)=>{
     res.end('Hello visitor, I have been here for ' + t + ' seconds!')
 })
 
-server.listen(3000, err => {
+server.listen(80, err => {
     if(err){
         return console.log(err)
     }
-    console.log('http://localhost:3000')
+    console.log('http://localhost')
 })
