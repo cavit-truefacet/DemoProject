f57e4482b22e0f8d4d150a9c5dd74a477112450e
Cavit Keskin <cavit@truefacet.com>
2017-12-08 15:50:23 -0500

    healty check

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
-const http = require('http')
+const express = require('express')
+const app = express()
+const os = require('os')
 
 var starttime = Date.now()
-const server = http.createServer((req, res, next)=>{
+var healthCheck = Date.now()
+var healthy = true
+
+app.get('/', (req, res, next) => {
     let t = (Date.now() - starttime) / 1000
-    res.end('Hello visitor, I have been here for ' + t + ' seconds!')
+    res.setHeader("Content-Type", 'text/html'); //Solution!
+    res.writeHead(200);
+    res.end('Hello visitor,<br>I have been here for ' + t + ' seconds!<br>' + os.hostname())
 })
 
-server.listen(80, err => {
-    if(err){
-        return console.log(err)
+app.get('/health', (req, res) => {
+    let t = (Date.now() - healthCheck) / 1000
+    if(t > 120){
+        healthCheck = Date.now()
+        healthy = !healthy
     }
-    console.log('http://localhost')
+    res.writeHead( healthy ? '200' : '500')
+    res.end()
 })
+
+app.listen(process.env.PORT||80, () => console.log('app is running on ' + (process.env.PORT||80)))
