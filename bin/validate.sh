46bcbbc6d6014a4d0a26af1b7bf445201815c444
Cavit Keskin <cavit@truefacet.com>
2017-12-08 16:18:58 -0500

    validate app is running

--- bin/validate.sh
+++ bin/validate.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-echo "VALIDATE APP INSTALL IS COMPLETE SUCCESS"
+echo "VALIDATE APP INSTALL IS SUCCESSFULL"
+
+STATUS=`curl -sL -w "%{http_code}\\n" "http://localhost/health" -o /dev/null`
+if [ "$STATUS" -ne "200" ]; then
+    echo "Not healty"
+    exit 1
+fi
 exit 0
