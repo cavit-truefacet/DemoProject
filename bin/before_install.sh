6b305585e90c81dd79b8a4de001c4c1e8f854418
Cavit Keskin <cavit@truefacet.com>
2017-12-08 11:41:43 -0500

    Update before_install.sh

--- bin/before_install.sh
+++ bin/before_install.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-echo "BEFORE INSTALL..."
-exit 0
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.6/install.sh | bash
+. ~/.nvm/nvm.sh
+nvm install 6.12.2
+nvm use 6.12.2
+npm i -g strongloop
